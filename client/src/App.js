28707783b5d8c7b4a21b7cb4e0f0b861ca42521b
Rostislav Král <vedeni@jktech.cz>
2022-02-05 21:45:26 +0100

    Fetching records from DB and rendering them in data grid

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,45 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Container} from "@mui/material";
 import useStyles from "./styles";
 import {AppBar, Paper, Typography} from "@material-ui/core";
 import {DataGrid} from "@mui/x-data-grid"
 import astronaut from "./images/astronaut.svg"
+import axios from "axios";
+import moment from "moment";
 
 const App = () => {
     const classes = useStyles();
 
+    const [astronauts, setAstronauts] = useState([])
+
+    useEffect(() => {
+        const getAstronauts = async () => {
+            const astronauts = await fetchAstronauts()
+            setAstronauts(astronauts)
+
+        }
+
+        getAstronauts()
+    }, [])
+
+
+    const fetchAstronauts = async () => {
+        try {
+            const {data} = await axios.get('http://localhost:5000/');
+
+            return data;
+        } catch (e) {
+            console.log(e)
+        }
+
+
+    }
+
+
+
+
     const columns = [
-        {field: 'id', headerName: 'ID', width: 70},
+        {field: '_id', headerName: 'ID', width: 240},
         {field: 'firstName', headerName: 'First name', width: 130},
         {field: 'lastName', headerName: 'Last name', width: 130},
         {
@@ -17,7 +47,7 @@ const App = () => {
             headerName: 'Age',
             type: 'number',
             width: 90,
-            valueGetter: (params) => `${params.row.birthDate}`
+            valueGetter: (params) => `${moment().diff(params.row.birthDate, 'years')}`
         },
         {
             field: 'fullName',
@@ -32,25 +62,21 @@ const App = () => {
             field: 'superPower',
             headerName: 'Superpower',
             width: 130,
+        },
+        {
+            headerName: 'Actions',
+            width: 100
         }
     ];
 
-    const rows = [
-        {id: 1, lastName: 'Snow', firstName: 'Jon', age: 35},
-        {id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42},
-        {id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45},
-        {id: 4, lastName: 'Stark', firstName: 'Arya', age: 16},
-        {id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null},
-        {id: 6, lastName: 'Melisandre', firstName: null, age: 150},
-        {id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44},
-        {id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36},
-        {id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65},
-    ];
+
+    const rows = astronauts.map((astronaut) => {
+        astronaut.id = astronaut._id
+        return astronaut;
+    });
 
     return (
         <Container maxWidth={"lg"}>
-
-
             <AppBar className={classes.appBar}>
                 <Typography className={classes.login} align={"right"}> LOGIN </Typography>
             </AppBar>
@@ -67,8 +93,6 @@ const App = () => {
                     checkboxSelection
                     components={Paper}
                     sx={{backgroundColor: "white"}}
-
-
                 />
             </div>
         </Container>
